a01a4e85177f19f183b003904c73262ffa5ae3da
karelhojda <hojda.k@gmail.com>
2025-10-13 09:09:20 +0200

    buttons refactor;

--- apps/client/src/components/organisms/Button/Button.tsx
+++ apps/client/src/components/organisms/Button/Button.tsx
@@ -3,21 +3,24 @@ import { tv, VariantProps } from 'tailwind-variants';
 import { ButtonHTMLAttributes, ReactNode } from 'react';
 
 const buttonVariants = tv({
-  base: 'rounded-2xl lg:rounded-3xl text-sm gap-5 lg:text-base flex items-center justify-center whitespace-nowrap cursor-pointer transition-colors duration-300',
+  base: 'rounded-2xl lg:rounded-3xl text-sm gap-5 lg:text-base flex items-center whitespace-nowrap cursor-pointer transition-colors duration-300',
   variants: {
     variant: {
       primary: 'border border-primary text-white bg-primary hover:bg-primary/80',
       secondary: 'bg-white text-gunmetal hover:bg-[#FEFEFE4D]',
       'primary-inverted': 'bg-white text-gunmetal',
       'secondary-inverted': 'border border-white text-white bg-gunmetal hover:bg-[#FEFEFE4D]',
+      'outline-white': 'border border-white text-white bg-transparent hover:bg-white/10',
     },
     size: {
       none: '',
-      base: 'px-10 lg:px-12 py-5 lg:py-6',
+      base: 'px-10 lg:px-12 py-5',
+      narrow: 'px-4 py-5',
     },
     sizeWithIcon: {
       none: '',
       base: 'pl-1.5 lg:pl-2 pr-10 lg:pr-12 py-1.5 lg:py-2',
+      narrow: 'pl-1.5 lg:pl-2 pr-4 lg:pr-8 py-1.5 lg:py-2',
     },
     iconPosition: {
       left: 'flex-row',
@@ -45,6 +48,7 @@ const iconVariants = tv({
       secondary: 'text-gunmetal bg-white',
       'primary-inverted': 'text-white bg-primary',
       'secondary-inverted': 'text-white bg-gunmetal',
+      'outline-white': 'text-white bg-transparent',
     },
   },
 });
@@ -80,7 +84,7 @@ export default function Button({
       {...props}
     >
       {hasIcon ? <span className={iconVariants({ variant })}>{icon}</span> : null}
-      {children}
+      {children ? <span className="flex-1">{children}</span> : null}
     </button>
   );
 }
